3d3faca2889d0cc34c698c7a4a9ef564a098106d
mobarokman <mobarokman@gmail.com>
2019-11-02 12:56:15 +0600

    update app

---
+++ precache-manifest.d04e249e85a2831fd01cb70021ed08da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2696b6941d49891159db06fed6694f86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74419f4bbedd0cc62c5b",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a88869d49c4bc628fb1c",
+    "url": "/static/css/main.66ae2f0e.chunk.css"
+  },
+  {
+    "revision": "74419f4bbedd0cc62c5b",
+    "url": "/static/js/2.f07c43e6.chunk.js"
+  },
+  {
+    "revision": "a88869d49c4bc628fb1c",
+    "url": "/static/js/main.08e85d62.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed7a83d46997a07db15354851dc6d91d",
+    "url": "/static/media/butterfly.ed7a83d4.jpg"
+  },
+  {
+    "revision": "0a481de8896ccd74af12fc787ac15ae4",
+    "url": "/static/media/mobarok.0a481de8.jpg"
+  }
+]);
